6b0c42853a11014d30f541778da9092c65d6731e
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2024-04-19 21:40:22 +0200

    user storie 2: as a user i want to have a menu to choose between making a movie and quitting. after creating a movie the menu should show again (implemented)

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        menu();
+    }
+    public static void menu(){
         Scanner input = new Scanner(System.in);
         MovieCollection collection = new MovieCollection();
-        System.out.println("title");
-        String title = input.nextLine();
-        System.out.println("director");
-        String director = input.nextLine();
-        System.out.println("color(yes/no)");
-        boolean color = input.nextBoolean();
-        System.out.println("Length of film(Min)");
-        int lengthInMinutes = input.nextInt();
-        System.out.println("year made");
-        int yearMade = input.nextInt();
-        System.out.println("genre");
-        input.nextLine();
-        String genre = input.nextLine();
-        collection.addMovie(new Movie(title, director, color, lengthInMinutes, yearMade, genre));
+        String userChoice = "";
+        while(!userChoice.equals("2")){
+            System.out.println("1. Create a movie");
+            System.out.println("2. Qut");
+            userChoice = input.next();
+            input.nextLine();
+            if(userChoice.equals("1")){
+                System.out.println("title");
+                String title = input.nextLine();
+                System.out.println("director");
+                String director = input.nextLine();
+                System.out.println("color(yes/no)");
+                boolean color = input.nextBoolean();
+                System.out.println("Length of film(Min)");
+                int lengthInMinutes = input.nextInt();
+                System.out.println("year made");
+                int yearMade = input.nextInt();
+                System.out.println("genre");
+                input.nextLine();
+                String genre = input.nextLine();
+                collection.addMovie(new Movie(title, director, color, lengthInMinutes, yearMade, genre));
+            }
+        }
     }
 }
